2b07a1f963923af2676251ca2ef37123d489a5ea
Yhuley <yuriihuley@gail.com>
2021-05-25 12:31:38 +0300

    account screens button added

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { AccountBackground, AccountCover } from "../components/account.styles";
 
-const AccountScreen = () => {
+import { Spacer } from "../../../components/spacer/spacer.component";
+import {AccountBackground, AccountContainer, AccountCover, AuthButton} from "../components/account.styles";
+
+const AccountScreen = ({ navigation }) => {
 
     return (
         <AccountBackground>
             <AccountCover/>
+            <AccountContainer>
+                <AuthButton
+                    icon="login"
+                    mode="contained"
+                    onPress={() => navigation.navigate("Login")}
+                >
+                    Log In
+                </AuthButton>
+                <Spacer size="large">
+                    <AuthButton
+                        icon="account-plus"
+                        mode="contained"
+                        onPress={() => navigation.navigate("Register")}
+                    >
+                        Register
+                    </AuthButton>
+                </Spacer>
+            </AccountContainer>
         </AccountBackground>
     );
 }
